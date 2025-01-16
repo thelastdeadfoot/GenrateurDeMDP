50724ffa6b83119e5a35a629e699b53f7b052d9b
Djordjevic <152289728+thelastdeadfoot@users.noreply.github.com>
2025-01-16 13:28:26 +0100

    Add files via upload

---
+++ FichierDao/MotDePasseDao.py
@@ -0,0 +1,70 @@
+import os
+from supabase import create_client, Client
+
+# Définir les clés d'environnement (faites ceci une fois, ou configurez-les dans votre système)
+os.environ["SUPABASE_URL"] = "https://vijrfostiknzlxhbsgwy.supabase.co"
+os.environ["SUPABASE_KEY"] = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpanJmb3N0aWtuemx4aGJzZ3d5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY4NDc5NjgsImV4cCI6MjA1MjQyMzk2OH0.w3FWw1e6859mozW_I89UfxglgaEdnrIQFfnDzbCuj8g"
+
+# Récupérer l'URL et la clé à partir des variables d'environnement
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+
+# Créer le client Supabase
+supabase: Client = create_client(url, key)
+
+class MotDePasseDao:
+    def __init__(self):
+        print("Instance de MotDePasseDao créée.")
+
+    # Insertion d'un seul mot de passe
+    def insertMdp(self, nbCaractere, nbNum, nbCarSpe, idSite, mdp, categorie, idUtilisateur):
+        print("Insertion de donnée mdp : " +mdp)
+        data = {"nbCaractere": nbCaractere, "nbNum": nbNum, "nbCarSpe": nbCarSpe, "idSite": idSite, "mdp": mdp, "categorie": categorie, "idUtilisateur" : idUtilisateur} 
+        response = supabase.table("MotDePasse").insert(data).execute()
+        print(response.data)
+        return response.data
+        
+    # Insertion de mot de passe via une liste
+    def insertMdpList(self, data):
+        print("Insertion des données mdp")
+        response = supabase.table("MotDePasse").insert(data).execute()
+        #ci-dessous, permet d'inserer des elements en excluant les doublons se trouvant dans la liste
+        # response = supabase.table("MotDePasse").upsert(data).execute()
+        print(response.data)
+        return response.data
+
+    # Récupérer toutes les mots de passe
+    def recupAllMdp(self):
+        print("Récupération des données mdp")
+        response = supabase.table("MotDePasse").select("*").execute()
+        print(response.data)
+        return response.data
+        
+    # Récupérer les mots de passe selon une colonne et une valeur 
+    # (par exemple, pour recuperer tout les catégorie qui sont égale a "Professionnel")
+    def recupMdpColonne(self, colonne, valeur):
+        print("Récupération des données mdp de valeur : "+valeur+"\n colonne : "+colonne)
+        response = supabase.table("MotDePasse").select("*").eq(colonne, valeur).execute()
+        print(response.data)
+        return response.data
+
+    # Récupérer un mot de passe
+    def recupOneMdp(self, mdpAchercher):
+        print("Récupération de la donnée "+ mdpAchercher)
+        response = supabase.table("MotDePasse").select("mdp").eq("mdp", mdpAchercher).execute()
+        print(response.data)
+        return response.data
+
+    # Met a jour un mot de passe
+    def updateMdp(self, mdpAmodifier, mdpModifier):
+        print("Mise à jour de la donnée mdp : "+ mdpAmodifier)
+        response = supabase.table("MotDePasse").update({"mdp": mdpModifier}).eq("mdp", mdpAmodifier).execute()
+        print(response.data)
+        return response.data
+
+    # Supprimer un mot de passe (marche aussi avec une liste de mdp)
+    def supMdp(self, mdpAsup):
+        print("Suppression des données mdp")
+        response = supabase.table("MotDePasse").delete().in_("mdp", mdpAsup).execute()
+        print(response.data) 
+        return response.data   
---
+++ FichierDao/main.py
@@ -0,0 +1,105 @@
+from MotDePasseDao import MotDePasseDao
+from SiteDao import SiteDao
+from UtilisateurDao import UtilisateurDao
+
+def main():
+   
+ #___________________PARTIE MOT DE PASSE__________________________________________________
+    #creer l'objet MotDePasseDao
+    mdpDao=MotDePasseDao()
+
+    #insert un seul mot de passe dans la base de données
+    mdpDao.insertMdp(5, 2, 1, 1, "C5u!3", "Professionnel", 1)
+
+    data = [
+    {"nbCaractere": "6", "nbNum": "3", "nbCarSpe": "2", "idSite": 1, "mdp": "abc123!", "categorie": "Professionnel", "idUtilisateur": 1},
+    {"nbCaractere": "8", "nbNum": "2", "nbCarSpe": "1", "idSite": 2, "mdp": "password$", "categorie": "Professionnel", "idUtilisateur": 1},
+    {"nbCaractere": "5", "nbNum": "1", "nbCarSpe": "0", "idSite": 3, "mdp": "qwerty", "categorie": "Personnel", "idUtilisateur": 1},
+    {"nbCaractere": "10", "nbNum": "4", "nbCarSpe": "3", "idSite": 4, "mdp": "secureP@ss", "categorie": "Personnel", "idUtilisateur": 1}]
+
+    #insert une liste dans la base de données
+    mdpDao.insertMdpList(data)
+
+    #recupere tout
+    mdpDao.recupAllMdp()
+
+    #recupere tout les mot de passe selon une colonne et une valeur 
+    #ici, on cherche tout les mot de passe qui sont categoriser ""Professionnel
+    mdpDao.recupMdpColonne("categorie", "Professionnel")
+
+    #permet de recuperer un mot de passe choisie
+    mdpDao.recupOneMdp("abc123!")
+
+    #met a jour un mot de passe
+    #abc123! devient simplement b
+    mdpDao.updateMdp("abc123!", "b")
+
+    #supprime un mot de passe
+    mdpDao.supMdp(["C5u!3"])   
+    
+    dataAsup=["b", "password$", "qwerty", "secureP@ss"]
+    #permet de supprimer un mot de passe via une liste
+    mdpDao.supMdp(dataAsup)
+
+    #_______________________PARTIE SITE___________________________
+    #creer l'objet SiteDao
+    siteDao = SiteDao()
+    
+    #insert un site
+    siteDao.insertSite("FaireDuPoridgeBonEtPasCher")
+
+    data = [
+    {"nomSite" : "Google"},
+    {"nomSite" : "Facebook"},]
+    #insert plusieurs site grace a une liste
+    siteDao.insertSiteList(data)
+       
+    #met a jour un site
+    siteDao.updateSite("FaireDuPoridgeBonEtPasCher", "FaireDuPoridge")
+
+    #recuperer tout les site
+    siteDao.recupAllSite()
+
+    #recuper un seul site
+    siteDao.recupOneSite("Facebook")
+
+    #supprime un site
+    siteDao.supSite(["FaireDuPoridge"])
+
+    dataAsup=["Google", "Facebook"]
+    #supprime un site via une liste
+    siteDao.supSite(dataAsup)
+
+    #____________________________PARTIE UTILISATEUR________________________________
+    #Crée l'objet qui te permettras d'intereagire avec la base de données
+    userDao= UtilisateurDao()
+       
+    #Exemple d'utilisation pour inserer un utilisateur
+    userDao.insertUser("dude", "LeMotDePasseDeDude")
+
+    data = [
+    {"login" : "guys", "mdpMaitre" : "guysPassword"},
+    {"login" : "mec", "mdpMaitre" : "leMdpDuMec"},]
+    #Exemple pour inserer plusieurs utilisateur via une liste (inutile vu notre app, mais au cas ou)
+    userDao.insertUserList(data)
+
+    #Recuperer la la liste de tout les utilisateurs
+    userDao.recupAllUser()
+
+    #Recuperer un seul utilisateur
+    userDao.recupOneUser("dude")
+
+    #Mettre un jour un mot de passe maitre d'un utilisateur 
+    userDao.updateMdp("guysPassword", "gp")
+
+    #Permet de mettre a jour un login d'un utilisateur
+    userDao.updateUser("guys", "guy")
+
+    #Permet de chercher un mot de passe selon un utilisateur
+    userDao.ChercheMdpSelonUser("mec")
+
+    #Permet de supprimer plusieurs ou 1 utilisateurs selon une liste 
+    userDao.supUser(["mec", "guy", "dude"])
+
+if __name__ == "__main__":
+    main()
