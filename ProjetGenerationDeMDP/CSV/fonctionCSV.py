787d8ea6146a1c978cdd34370bfcb222f0a94332
Djordjevic <152289728+thelastdeadfoot@users.noreply.github.com>
2025-02-13 18:43:30 +0100

    Update fonctionCSV.py

--- ProjetGenerationDeMDP/CSV/fonctionCSV.py
+++ ProjetGenerationDeMDP/CSV/fonctionCSV.py
@@ -1,5 +1,7 @@
+import base64
 import io
 import os
+import threading
 
 import pandas as pd
 import psycopg2
@@ -17,23 +19,60 @@ class fonctionCSV:
         self.fichierCSV = fichierCSV
         print("fonctionCSV crée")
 
+    def pop_up(self, message, tk,duration):
+        toast = tk.Toplevel()
+        toast.overrideredirect(True)
+        # Pour qu'il soit toujours au premier plan
+        toast.attributes('-topmost', True)
+
+        # Obtenir la position de l'écran
+        screen_width = toast.winfo_screenwidth()
+        screen_height = toast.winfo_screenheight()
+
+        # La taille et la position du toast
+        toast_width = 330
+        toast_height = 80
+        x_position = (screen_width - toast_width) // 2
+        y_position = screen_height - toast_height - 50
+
+        toast.geometry(f"{toast_width}x{toast_height}+{x_position}+{y_position}")
+
+        # C'est pour mettre le message
+        label = tk.Label(toast, text=message, bg="white", fg="black", padx=10, pady=5)
+        label.pack(expand=True, fill='both')
+
+        # Fermer la popup apres la fin du temps
+        toast.after(duration, toast.destroy)
+
+        return toast
+
     # La fonction qui permet d'inserer les données dans la base de données
-    def load_data(self, lectureFichier, nomTable, nomDonnee):
+    def load_data(self, tk,lectureDataFrame, nomTable, nomDonnee):
         # connexion a la base de données
-        # probleme, ne marche qu'avec un reseau en IPv6
+        # probleme, ne marche qu'avec un reseau en IPv6 (on va gentiment ignorer ce probleme...)
         con = psycopg2.connect(
             "postgresql://postgres:bZ60rQPU8FHKHjwi@db.vijrfostiknzlxhbsgwy.supabase.co:5432/postgres")
         cursor = con.cursor()
         try:
             print("Début de la phase d'insertion dans la base de donnée")
             csv_buffer = io.StringIO()
-            lectureFichier.to_csv(csv_buffer, index=False, header=False)
+            lectureDataFrame.to_csv(csv_buffer, index=False, header=False)
             csv_buffer.seek(0)
             # Requete qui permet d'inserer tout les données avec un csv
             copySql = f'COPY "{nomTable}"({nomDonnee}) FROM STDOUT WITH CSV'
             cursor.copy_expert(copySql, csv_buffer)
             con.commit()
             print("Nbre de lignes enregistrés {}".format(cursor.rowcount))
+            #popup qui montre que le mdp a bien été envoyée, je le decrypte avant de lui envoyée
+            #je suis obliger de faire une copy de mon dataFrame, car sinon, mes element se modifie et se decrypte definitivement
+            chifr = Chiffrement()
+            copyDf = lectureDataFrame.copy()
+            print(copyDf["mdp"])
+            copyDf["mdp"] = copyDf["mdp"].apply(lambda detruit: detruit[2:])
+            print(copyDf["mdp"])
+            copyDf["mdp"] = copyDf["mdp"].apply(lambda decrypt: chifr.decrypteMDP(bytes.fromhex(decrypt)))
+            print(copyDf["mdp"])
+            self.pop_up(f"{copyDf['mdp'].iloc[0]}\nà bien été envoyée vers la base de données ✅", tk, 2000)
         except Exception as e:
             print("Une erreur est survenue pendant la phase d'insertion {}".format(e))
         finally:
@@ -41,13 +80,9 @@ class fonctionCSV:
             con.close()
             print("Fermeture de la connexion")
 
-    #Permet de copier le fichiers
-    #je le fait car l'utilisateur ne veux pas que son fichier csv se fasse modifier (et surtout ça marche pas sinon)
-    def saveToFile(self, data):
-        data.to_csv('mdpAfterChangeCSV.csv', encoding='utf-8', index=False, header=False)
-        print("Le fichier a été généré avec le chemin suivant ; {}".format('mdpAfterChangeCSV.csv'))
-        filesave = open('mdpAfterChangeCSV.csv', "r")
-        return filesave
+    def load_data_thread(self, df, tk, nomTable, nomDonee):
+        thread = threading.Thread(target=self.load_data, args=(tk, df, nomTable, nomDonee))
+        thread.start()
 
     #Tout la manipulation du fichier csv
     def extraction(self, NomFichier, tk, cadre_contenu):
@@ -122,22 +157,22 @@ class fonctionCSV:
                 site.insertSite(verifSite)
         # et si il existe, il recupere l'identifiant du site et le met dans la colonne idSite crée auparavant
         df["idSite"] = df["idSite"].apply(lambda site_id: site.recupIdSite(site_id))
-
-        #df["idUtilisateur"] = userDao.recupIdUtilisateur(user.getLogin)
-        df["idUtilisateur"] = 125
+        print(user.getLogin())
+        df["idUtilisateur"] = userDao.recupIdUtilisateur(user.getLogin())
+        print(df["idUtilisateur"])
+        #df["idUtilisateur"] = 125
 
         hexConvert = df["mdp"].apply(lambda crytp : chifr.crypteMDP(crytp).hex())
         df["mdp"] = "\\x" + hexConvert
         print(df.to_string(max_rows=20))
 
-        tk.Button(frame, text=f"Envoyer MDP", command=lambda ligne=ligne_df: self.load_data(df, "MotDePasse",'"mdp", "idSite", "categorie", "nbCaractere", "nbNum", "nbCarSpe", "idUtilisateur", "Robustesse", "carMini", "carMaj"')).grid(row=0, column=2, padx=1, pady=3, sticky="nsew")
+        tk.Button(frame, text=f"Envoyer MDP", command=lambda ligne=ligne_df: self.load_data_thread(df, tk,"MotDePasse",'"mdp", "idSite", "categorie", "nbCaractere", "nbNum", "nbCarSpe", "idUtilisateur", "Robustesse", "carMini", "carMaj"')).grid(row=0, column=1, padx=1, pady=3, sticky="nsew")
 
         for index, row in df.iterrows():
             print("la, on est rentré dans le for")
             ligne_df = pd.DataFrame([row])
             print(ligne_df)
-            tk.Button(frame, text=f"Envoyer MDP (Ligne {index})", command=lambda ligne=ligne_df: self.load_data(ligne,"MotDePasse",'"mdp", "idSite", "categorie", "nbCaractere", "nbNum", "nbCarSpe", "idUtilisateur", "Robustesse", "carMini", "carMaj"')).grid(row=index+1, column=1, padx=1, pady=3, sticky="nsew")
-
+            tk.Button(frame, text=f"Envoyer MDP (Ligne {index})", command=lambda ligne=ligne_df: self.load_data_thread(ligne, tk,"MotDePasse",'"mdp", "idSite", "categorie", "nbCaractere", "nbNum", "nbCarSpe", "idUtilisateur", "Robustesse", "carMini", "carMaj"')).grid(row=index+1, column=2, padx=1, pady=3, sticky="nsew")
         return df
 
     def envoieMdpViaCSV(self, tk, cadre_contenu):
